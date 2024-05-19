902b4c6ee7c2f2385c1741885253351671895e87
muhammadmedhat99 <muhammadmedhat12@gmail.com>
2024-05-20 00:48:50 +0300

    error

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 
-import { companies, testimonials } from "@/data";
+import { companies } from "@/data";
 import { InfiniteMovingCards } from "./ui/InfiniteCards";
 
 const Clients = () => {
@@ -18,11 +18,11 @@ const Clients = () => {
           // remove bg-white dark:bg-black dark:bg-grid-white/[0.05], h-[40rem] to 30rem , md:h-[30rem] are for the responsive design
           className="h-[50vh] md:h-[30rem] rounded-md flex flex-col antialiased  items-center justify-center relative overflow-hidden"
         >
-          <InfiniteMovingCards
+          {/* <InfiniteMovingCards
             items={testimonials}
             direction="right"
             speed="slow"
-          />
+          /> */}
         </div>
 
         <div className="flex flex-wrap items-center justify-center gap-4 md:gap-16 max-lg:mt-10">
